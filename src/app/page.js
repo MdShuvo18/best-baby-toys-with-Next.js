1467947b9b76694a26777c4a19bbe41d4fd6f406
MdShuvo18 <mdks33881@gmail.com>
2024-12-12 17:02:35 +0600

    initial navbar added with logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
+import HomePage from "@/components/HomePage/HomePage";
 
 
 export default function Home() {
   return (
-  
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        best baby toys
-      </main>
-      
+
+    <main className="">
+      <HomePage />
+    </main>
+
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default HomePage;
